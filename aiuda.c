2eb0125d5c52dc0c811d5536588f239d56d12a3b
Panesico <panesico@localhost.localdomain>
2023-05-04 17:05:57 +0200

    aiuda

--- aiuda.c
+++ aiuda.c
@@ -1,79 +1,174 @@
+# include <unistd.h>
+# include <stdlib.h>
 #include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include "stdio.h"
-
+#include <stdio.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 32
+#  define BUFFER_SIZE 6
 # endif
 
-typedef struct s_fd_str
+unsigned int	ft_read(int fd);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_buffer(char *str, int boole);
+char			*ft_strdup(const char *s);
+size_t			ft_strlen(const char *str);
+char			*ft_strchr(const char *str, int c);
+void			*ft_calloc(size_t count, size_t size);
+
+void	*ft_calloc(size_t count, size_t size)
 {
-	char	*str;
-	int 	fd;
-}	t_fd_str;
-
-
-unsigned int	ft_read(t_fd_str *fd);
-t_fd_str	*ft_new_fd(int filed);
-void	ft_free_fd_struct(t_fd_str	*fd_struct);
-
-unsigned int	ft_read(t_fd_str *fd)
-{
-	char	c;
-	static int	i;
+	char	*ptr;
+	size_t	i;
 
 	i = 0;
-	while(i < BUFFER_SIZE && read(fd->fd, &c, 1))
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	while (i < count * size)
 	{
-		if(c == '\n')
-		{
-			fd->str[i++] = c;
-			fd->str[i++] = '\0';
-			return (1);
-		}
-		else
-			fd->str[i++] = c;
+		((char *)ptr)[i] = 0;
+		i++;
 	}
-	if(fd->str[0])
-		return(fd->str[i] = 0, 1);
+	return (ptr);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t	i;
+	char	*str;
+
+	str = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (!str)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	size_t	i;
+
+	if(!str)
+		return (0);
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] == (char)c)
+			return ((char *)&str[i]);
+	}
+	if (str[i] == (char)c)
+		return ((char *)&str[i]);
 	return (0);
 }
 
-t_fd_str	*ft_new_fd(int filed)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	t_fd_str	*fd_struct;
+	size_t	i;
+	size_t	j;
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*str;
 
-	fd_struct = (t_fd_str *)malloc(sizeof(t_fd_str));
-	fd_struct->fd = filed;
-	fd_struct->str = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (!str)
+		return (0);
+	i = 0;
+	j = 0;
+	while (j < s1_len && s1[j])
+		str[i++] = s1[j++];
+	j = 0;
+	while (j < (s1_len + s2_len) && s2[j] && s2[j] != '\n')
+		str[i++] = s2[j++];
+	if (s2[j] == '\n')
+		str[i++] = s2[j++];
+	str[i] = 0;
+	return (str);
+}
+char	*ft_buffer(char *str, int boole)
+{
+	static char	*str2;
+	char		*temp;
+	static int	boolea;
 
-	return (fd_struct);
+	if (!boole)
+		return (0);
+	if (boole == 2)
+	{
+		str2 = ft_strdup(str);
+		boolea = 1;
+		return (0);
+	}
+	if (boole == 1)
+		return (str2);
+	if(boolea--)
+	{
+		temp = ft_strjoin(str2, str);
+		free(str2);
+		return(str2 = temp);
+	}
+	str2 = ft_strjoin(str2, str);
+	return (str2);
 }
 
-void	ft_free_fd_struct(t_fd_str	*fd_struct)
+unsigned int	ft_read(int fd)
 {
-	free(fd_struct);
+	char		*str;
+	static int	i;
+
+	str = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	i = 0;
+	while(!ft_strchr(ft_buffer(str, i), '\n'))
+	{
+		if(!(read(fd, str, BUFFER_SIZE)))
+			break ;
+		str[BUFFER_SIZE] = 0;
+		if(!i)
+		{
+			ft_buffer(str, 2);
+			i++;
+		}
+		else
+			ft_buffer(str, 3);
+	}
+	if(str[0])
+		return(1);
+	return (0);
 }
 
 char *ft_get_next_line(int fd)
 {
 	char		*str;
-	t_fd_str	*fd_struct;
 
 	if(fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	fd_struct = ft_new_fd(fd);
-	ft_read(fd_struct);
-	str = fd_struct->str;
+	if(!ft_read(fd))
+		return (0);
+	str = ft_buffer(0, 1);
 	return (str);
 }
 
 int main()
 {
-	int i = 0;
-	int fd = open("hola", 0);
-	while(i++ != 10)
-		printf("%s", ft_get_next_line(fd));
+	int i = open("hola", 0);
+	int j = 3;
+	while(j--)
+		printf(ft_get_next_line(i));
 }
